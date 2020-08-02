468aea6d50c923a7dfdf020f5ac8406430568da1
{yusukenishimori} <{yusukenishimori777@gmail.com}>
2020-08-02 09:03:14 +0900

    ex25.py added

---
+++ ex25.py
@@ -0,0 +1,32 @@
+def break_words(stuff):
+	"""この関数は、文章を単語に分解する"""
+	words=stuff.split(' ')
+	return words
+
+def sort_words(words):
+	"""単語を並べ替える"""
+	return sorted(words)
+
+def print_first_word(words):
+	word=words.pop(0)
+	print(word)
+
+def print_last_word(words):
+	word=words.pop(-1)
+	print(word)
+
+def sort_sentence(sentence):
+	words=break_words(sentence)
+	return sort_words(words)
+
+def print_first_and_last(sentence):
+	words=break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	words=sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+
